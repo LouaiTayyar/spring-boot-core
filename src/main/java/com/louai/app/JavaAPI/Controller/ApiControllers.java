95379cf48531f6dc9e6365dcb85715fc35a2a10d
louaitayyar <louaitayyar@outlook.com>
2022-12-01 14:53:01 +0200

    Final commit

--- src/main/java/com/louai/app/JavaAPI/Controller/ApiControllers.java
+++ src/main/java/com/louai/app/JavaAPI/Controller/ApiControllers.java
@@ -3,8 +3,7 @@ package com.louai.app.JavaAPI.Controller;
 import com.louai.app.JavaAPI.Models.Repo.UserRepo;
 import com.louai.app.JavaAPI.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +20,29 @@ public class ApiControllers {
     @GetMapping(value = "/users")
     public List<User> getUsers() {
         return userRepo.findAll();
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestBody User user){
+        userRepo.save(user);
+        return "Saved";
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public String updateUser(@PathVariable long id, @RequestBody User user){
+        User updatedUser = userRepo.findById(id).get();
+        updatedUser.setFirstName(user.getFirstName());
+        updatedUser.setLastName(user.getLastName());
+        updatedUser.setAge(user.getAge());
+        updatedUser.setOccupation(user.getOccupation());
+        userRepo.save(updatedUser);
+        return "Updated";
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable long id){
+        User deleteUser = userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
+        return "Deleted";
     }
 }
